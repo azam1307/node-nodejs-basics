34077128c63450189000b088c8554b869ce6e666
Abdusalomov Azam <116247084+azam1307@users.noreply.github.com>
2022-12-02 23:16:13 +0500

    create.js Done!

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { access, constants, writeFile } from "node:fs";
+const file = "fresh.txt";
 const create = async () => {
-    // Write your code here 
+  access(file, constants.F_OK, (err) => {
+    if (!err) {
+      throw new Error("FS operation failed");
+    } else {
+      writeFile(file, "I am fresh and young", (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+  });
 };
 
 await create();

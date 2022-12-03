b29cd12b9d65401dcf366572bed2b53af87fd76e
Abdusalomov Azam <116247084+azam1307@users.noreply.github.com>
2022-12-03 15:06:07 +0500

    copy.js Done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import * as url from "url";
+import { access, copyFile, mkdir, readdir } from "fs/promises";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const srcFolder = __dirname + "/files";
+const copyFolder = __dirname + "/files_copy";
+const errMsg = "FS Operation Failed";
 const copy = async () => {
-    // Write your code here 
+  try {
+    await access(copyFolder);
+    throw new Error(errMsg);
+  } catch (err) {
+    try {
+      if (err.message === errMsg) {
+        throw new Error(err);
+      }
+      const srcFiles = await readdir(srcFolder);
+      await mkdir(copyFolder);
+      await Promise.all(
+        srcFiles.map((file) => {
+          copyFile(`${srcFolder}/${file}`, `${copyFolder}/${file}`);
+        })
+      );
+      console.log(`Success`);
+    } catch (err) {
+      throw new Error(errMsg);
+    }
+  }
 };
 
-copy();
+await copy();

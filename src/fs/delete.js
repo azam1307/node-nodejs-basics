9cca33a73a285bf109c345c5d791fc4d0b285b39
Abdusalomov Azam <116247084+azam1307@users.noreply.github.com>
2022-12-03 15:35:02 +0500

    delete Done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+import { access, unlink } from "fs/promises";
+const file = __dirname + "/files/fileToRemove.txt";
+const errMsg = "FS operation failed";
 const remove = async () => {
-    // Write your code here 
+  try {
+    await access(file);
+
+    await unlink(file);
+    console.log("Success");
+  } catch (err) {
+    throw new Error(errMsg);
+  }
 };
 
 await remove();

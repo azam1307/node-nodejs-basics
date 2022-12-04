bfa034b861d2af109c15b75870922dd996ffba75
Abdusalomov Azam <116247084+azam1307@users.noreply.github.com>
2022-12-04 22:40:44 +0500

    Read Done!

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+import { readFile } from "fs/promises";
+const errMsg = "FS Operation Failed";
+const file = __dirname + "/files/fileToRead.txt";
 const read = async () => {
-    // Write your code here 
+  try {
+    const res = await readFile(file, { encoding: "utf-8" });
+    console.log(res);
+  } catch (err) {
+    throw new Error(errMsg);
+  }
 };
 
 await read();

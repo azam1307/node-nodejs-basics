cd6901e6d5486c71b927036bd9cd3e613927157d
Abdusalomov Azam <116247084+azam1307@users.noreply.github.com>
2022-12-04 22:49:18 +0500

    Rename Done!

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+import * as fs from 'fs/promises'
+const errMsg = "FS Operation Failed";
+const filePath = __dirname + "/files/wrongFilename.txt";
+const newFile = __dirname + "/files/properFilename.md";
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fs.access(filePath);
+    await fs.rename(filePath, newFile);
+    console.log(`Success`);
+  } catch (err) {
+    throw new Error(errMsg);
+  }
 };
 
 await rename();

2f4918d28588f34c263513ea7c8543f53f0fb975
Abdusalomov Azam <116247084+azam1307@users.noreply.github.com>
2022-12-05 09:16:23 +0500

    ENV Done!

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  let result = Object.entries(process.env);
+  result.forEach((value) => {
+    value.reduce((prev, current) => {
+      if (prev.startsWith("RSS_")) {
+        console.log(`${prev} = ${current};`);
+      }
+    });
+  });
 };
 
 parseEnv();
